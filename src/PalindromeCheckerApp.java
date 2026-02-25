82f43bf6e18d006a45df7df4e453deaa7f802a5f
Xavios-42 <anushmishra2006@gmail.com>
2026-02-25 16:24:12 +0530

    UC8: Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,60 @@
-import java.util.Deque;
-import java.util.LinkedList;
 public class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
     public static void main(String[] args) {
-        String word = "civic";
+        String word = "refer";
         System.out.println("Checking word: " + word);
-        Deque<Character> deque = new LinkedList<>();
-        for (int i = 0; i < word.length(); i++) {
-            deque.add(word.charAt(i));
-        }
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        Node head = buildLinkedList(word);
+        boolean isPalindrome = checkPalindrome(head);
         if (isPalindrome) {
             System.out.println("Result: \"" + word + "\" is a palindrome.");
         } else {
             System.out.println("Result: \"" + word + "\" is NOT a palindrome.");
         }
+    }
+    static Node buildLinkedList(String word) {
+        Node head = new Node(word.charAt(0));
+        Node current = head;
+        for (int i = 1; i < word.length(); i++) {
+            current.next = new Node(word.charAt(i));
+            current = current.next;
+        }
+        return head;
+    }
+    static boolean checkPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverseList(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
     }
 }
